396743ab9dcb935b39510279eb803317a3dc325b
darrell <jafet.dna@gmail.com>
2023-08-12 23:14:21 -0600

    ...

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: "./src/index.js",
     customize: "./src/customization.js",
@@ -8,5 +9,19 @@ module.exports = {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 5500,
+    proxy: {
+      '/api': {
+        target: 'https://qfble0gquj.execute-api.us-east-2.amazonaws.com/',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/plant-store',
+        },
+      },
+    },
   },
 };
